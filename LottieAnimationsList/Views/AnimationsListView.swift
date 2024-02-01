b5f383cf5a758c3c93025a9dc88d3d27442a06c2
Xinlin Zhou <XZhou@lcs.on.ca>
2024-01-31 21:14:57 -0500

    Organized project and created a list view to show interesting animations.

---
+++ LottieAnimationsList/Views/AnimationsListView.swift
@@ -0,0 +1,18 @@
+//
+//  AnimationsListView.swift
+//  LottieAnimationsList
+//
+//  Created by Fiona ZHOU on 2024-01-31.
+//
+
+import SwiftUI
+
+struct AnimationsListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnimationsListView()
+}
