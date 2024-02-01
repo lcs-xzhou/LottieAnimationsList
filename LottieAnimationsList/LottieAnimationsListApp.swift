03415f6ce3038387bf0fbf8247e9dd38f4656975
Xinlin Zhou <XZhou@lcs.on.ca>
2024-01-31 21:12:06 -0500

    Initial Commit

---
+++ LottieAnimationsList/LottieAnimationsListApp.swift
@@ -0,0 +1,17 @@
+//
+//  LottieAnimationsListApp.swift
+//  LottieAnimationsList
+//
+//  Created by Fiona ZHOU on 2024-01-31.
+//
+
+import SwiftUI
+
+@main
+struct LottieAnimationsListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
